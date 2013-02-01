e8a2dd539b9d9b84bd3eb68f21eb17346d1c2331
ikarosilva00@gmail.com <ikarosilva00@gmail.com@8a878f76-cdcb-badc-6536-4bb3146487dd>
2013-02-01 18:28:02 +0000

    fixed path/exit problems in main.

--- trunk/src/org/audiopulse/analysis/DPOAEAnalysis.java
+++ trunk/src/org/audiopulse/analysis/DPOAEAnalysis.java
@@ -49,25 +49,26 @@ public class DPOAEAnalysis {
 
 		for(int i=0;i<oaeFiles.length;i++){
 
-			System.out.println("Analzing: " + oaeFiles[i]); 
+			System.out.println("Analyzing raw file: " + oaeFiles[i]); 
 			double[] XFFT= DPOAEAnalysis.getSpectrum
 					(ShortFile.readFile(oaeFiles[i].getAbsolutePath()));
 			//Plot spectrum
 			String outFileName=oaeFiles[i].getAbsolutePath().replace(".raw","")+".png";
-
 			plotSpectrum("DPOAE",XFFT,Fs,Fres,outFileName);
 
 		}
 		//Plot Audiogram
 		//TODO: Extract these results from data!
+		System.out.println("Calculating DPGram Results...");
 		double[] DPOAEData={7.206, -7, 5.083, 13.1,3.616, 17.9,2.542, 11.5,1.818, 17.1};
 		double[] noiseFloor={7.206, -7-10,5.083, 13.1-10,3.616, 17.9-10,2.542, 11.5-10,1.818, 17.1-10};
 		double[] f1Data={7.206, 64,5.083, 64,3.616, 64,2.542, 64,1.818, 64};
 		double[] f2Data={7.206, 54.9,5.083, 56.6,3.616, 55.6,2.542, 55.1,1.818, 55.1};
 
-		String outFileName2="/home/ikaro/dpaudiogram.png";
+		String outFileName2=data_dir+"dpaudiogram.png";
 		PlotAudiogram audiogram=new PlotAudiogram("test",DPOAEData,noiseFloor,f1Data,f2Data,outFileName2);
-
+		System.out.println("Analysis complete! ");
+		System.exit(0);
 	}
 
 
