0f2ca55c09e97c632c393aa5ced05fff0a5e10ee
ikarosilva00@gmail.com <ikarosilva00@gmail.com@8a878f76-cdcb-badc-6536-4bb3146487dd>
2013-02-02 22:44:38 +0000

    Added framework for obtaining results from AOE recording.

--- trunk/src/org/audiopulse/analysis/DPOAEAnalysis.java
+++ trunk/src/org/audiopulse/analysis/DPOAEAnalysis.java
@@ -76,40 +76,58 @@ public class DPOAEAnalysis {
 
 		//Get directory listing 
 		String data_dir=args[0];
-		double Fs=8000;
-		//double F2=2000;;
-		//double F1=F2/1.2;
-		double F2=2016;
-		double F1=1641;
-		double Fres=(2*F1)-F2;	 //Frequency of the expected response
+		double Fs=8000, F2=0, F1=0,Fres=0;	 //Frequency of the expected response
 		File[] oaeFiles=finder(data_dir);
 		Arrays.sort(oaeFiles);
-		double[][] results;
+		double[][] results; 
+		int M=3, fIndex=0; //number of frequencies being tested
+		//The data is sent for plotting in an interleaved fashion
+		//where odd elements are x-axis and even elements are y-axis
+		double[] DPOAEData=new double[2*M];
+		double[] noiseFloor=new double[2*M];
+		double[] f1Data=new double[2*M];
+		double[] f2Data=new double[2*M];
 		for(int i=0;i<oaeFiles.length;i++){
-
-			System.out.println("Analyzing raw file: " + oaeFiles[i]); 
+			String outFileName=oaeFiles[i].getAbsolutePath().replace(".raw","")+".png";		
+			//Determine which frequency was tested
+			//TODO: Right now the analysis is based on the Handbook of Otoacoustic Emissions Book by Hall
+			//These parameters (F1,F2,Fres) should be loaded dynamically based on the protocol description
+			//on the acompanying XML File
+			if(outFileName.contains("AP_DPOAE-2kHz")){
+				F2=2000;F1=F2/1.2;Fres=(2*F1)-F2;	
+				fIndex=0;
+			}else if(outFileName.contains("AP_DPOAE-3kHz")){
+				F2=3000;F1=F2/1.2;Fres=(2*F1)-F2;
+				fIndex=1*2;
+			}else if(outFileName.contains("AP_DPOAE-4kHz")){
+				F2=4000;F1=F2/1.2;Fres=(2*F1)-F2;
+				fIndex=2*2;
+			}else{
+				System.err.println("Unexpected DPOAE File Name!");
+			}
+			
 			double[][] XFFT= DPOAEAnalysis.getSpectrum
 					(ShortFile.readFile(oaeFiles[i].getAbsolutePath()),Fs);
 			//Plot spectrum
-			String outFileName=oaeFiles[i].getAbsolutePath().replace(".raw","")+".png";
 			plotSpectrum("DPOAE",XFFT,Fres,outFileName);
 			results=getResponse(XFFT,F1,F2,Fres);
-			System.out.println(" F1amp= " + results[1][1] + " ,F2amp= " 
-						+ results[1][1]	+  " ,Fres= " + results[1][2]);
+			
+			f1Data[fIndex]=Math.round(results[0][0]);
+			f1Data[fIndex+1]=Math.round(results[1][0]);
+			f2Data[fIndex]=Math.round(results[0][1]);
+			f2Data[fIndex+1]=Math.round(results[1][1]);
+			DPOAEData[fIndex]=Math.round(results[0][2]);
+			DPOAEData[fIndex+1]=Math.round(results[1][2]);
+			noiseFloor[fIndex]=Math.round(results[0][2]-10);
+			noiseFloor[fIndex+1]=Math.round(results[1][2]-10);
 		}
-		/*
-		//Plot Audiogram
-		//TODO: Extract these results from data!
-		System.out.println("Calculating DPGram Results...");
-		double[] DPOAEData={7.206, -7, 5.083, 13.1,3.616, 17.9,2.542, 11.5,1.818, 17.1};
-		double[] noiseFloor={7.206, -7-10,5.083, 13.1-10,3.616, 17.9-10,2.542, 11.5-10,1.818, 17.1-10};
-		double[] f1Data={7.206, 64,5.083, 64,3.616, 64,2.542, 64,1.818, 64};
-		double[] f2Data={7.206, 54.9,5.083, 56.6,3.616, 55.6,2.542, 55.1,1.818, 55.1};
-
-		String outFileName2=data_dir+"dpaudiogram.png";
-		PlotAudiogram audiogram=new PlotAudiogram("test",DPOAEData,noiseFloor,f1Data,f2Data,outFileName2);
+		
+		String outFileName2=data_dir+"dpaudiogram.png";		 		
+		System.out.println("f1=" + f1Data[0]+"f2=" + f2Data[0]+"res=" + DPOAEData[0]);
+		System.out.println("f1=" + f1Data[1]+"f2=" + f2Data[1]+"res=" + DPOAEData[1]);
+		System.out.println("f1=" + f1Data[2]+"f2=" + f2Data[2]+"res=" + DPOAEData[2]);
+		PlotAudiogram audiogram=new PlotAudiogram("DPGram",DPOAEData,noiseFloor,f1Data,f2Data,outFileName2);
 		System.out.println("Analysis complete! ");
-		*/
 		System.exit(0);
 	}
 
