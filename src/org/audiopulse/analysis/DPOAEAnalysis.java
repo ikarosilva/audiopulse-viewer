8631c7abc134722177a76abfe94d5e4fd6b0fbab
ikarosilva00@gmail.com <ikarosilva00@gmail.com@8a878f76-cdcb-badc-6536-4bb3146487dd>
2013-02-03 19:11:32 +0000

    made some improvements in how the amp stimulus was being calculated.

--- src/org/audiopulse/analysis/DPOAEAnalysis.java
+++ src/org/audiopulse/analysis/DPOAEAnalysis.java
@@ -26,38 +26,28 @@ public class DPOAEAnalysis {
 		demo.setVisible(true);
 	}
 
-	public static double[][] getResponse(double[][] XFFT, double F1, 
-			double F2, double Fres){
+	public static double[] getResponse(double[][] XFFT, double desF, double tolerance){
 	
 		//Search through the spectrum to get the closest bin 
 		//to the respective frequencies
-		double dminF1=Short.MAX_VALUE,
-				dminF2=Short.MAX_VALUE,dminFres=Short.MAX_VALUE;
-		double dF1, dF2,dFres;
-		//Results will be stored in a matrix where first row is the closest
+		double dminF=Short.MAX_VALUE;
+		double dF; 
+		//Results will be stored in a vector where first row is the closest
 		//bin from the FFT wrt the frequency and second row is the power in that
-		//bin. The columns are organized as F1, F2 and Fres
-		double[][] result=new double[2][3];
+		//bin. 
+		double[] result=new double[2];
 		for(int n=0;n<XFFT[0].length;n++){
-			dF1=Math.abs(XFFT[0][n]-F1);
-			dF2=Math.abs(XFFT[0][n]-F2);
-			dFres=Math.abs(XFFT[0][n]-Fres);		
-			if( dF1 < dminF1 ){
-				dminF1=dF1;
-				result[0][0]=n;
-				result[1][0]=XFFT[1][n];
-			}
-			if( dF2 < dminF2 ){
-				dminF2=dF2;
-				result[0][1]=n;
-				result[1][1]=XFFT[1][n];
-			}
-			if( dFres < dminFres ){
-				dminFres=dFres;
-				result[0][2]=n;
-				result[1][2]=XFFT[1][n];
-			}
-			
+			dF=Math.abs(XFFT[0][n]-desF);
+			if( dF < dminF ){
+				dminF=dF;
+				result[0]=n;
+				result[1]=XFFT[1][n];
+			}		
+		}
+		if(dminF > tolerance){
+			double  actF=XFFT[0][(int)result[0]];
+			System.err.println("Results are innacurate because frequency tolerance has been exceeded. Desired F= "
+					+ desF +" closest F= " + actF);
 		}
 		return result;
 	}
@@ -79,7 +69,8 @@ public class DPOAEAnalysis {
 		double Fs=8000, F2=0, F1=0,Fres=0;	 //Frequency of the expected response
 		File[] oaeFiles=finder(data_dir);
 		Arrays.sort(oaeFiles);
-		double[][] results; 
+		double[][] results = new double[2][3]; 
+		double tolerance=10; //Tolerance, in Hz, from which to get the closest FFT bin relative to the actual desired frequency
 		int M=3, fIndex=0; //number of frequencies being tested
 		//The data is sent for plotting in an interleaved fashion
 		//where odd elements are x-axis and even elements are y-axis
@@ -87,6 +78,8 @@ public class DPOAEAnalysis {
 		double[] noiseFloor=new double[2*M];
 		double[] f1Data=new double[2*M];
 		double[] f2Data=new double[2*M];
+		double[] tmpResult=new double[2];
+		
 		for(int i=0;i<oaeFiles.length;i++){
 			String outFileName=oaeFiles[i].getAbsolutePath().replace(".raw","")+".png";		
 			//Determine which frequency was tested
@@ -95,7 +88,7 @@ public class DPOAEAnalysis {
 			//on the acompanying XML File
 			if(outFileName.contains("AP_DPOAE-2kHz")){
 				F2=2000;F1=F2/1.2;Fres=(2*F1)-F2;	
-				fIndex=0;
+				fIndex=0*2;//index are all even, data amp goes in the odd indeces
 			}else if(outFileName.contains("AP_DPOAE-3kHz")){
 				F2=3000;F1=F2/1.2;Fres=(2*F1)-F2;
 				fIndex=1*2;
@@ -110,22 +103,35 @@ public class DPOAEAnalysis {
 					(ShortFile.readFile(oaeFiles[i].getAbsolutePath()),Fs);
 			//Plot spectrum
 			plotSpectrum("DPOAE",XFFT,Fres,outFileName);
-			results=getResponse(XFFT,F1,F2,Fres);
+			tmpResult=getResponse(XFFT,F1,tolerance);
+			results[0][0]=tmpResult[0];
+			results[1][0]=tmpResult[1];
 			
-			f1Data[fIndex]=Math.round(results[0][0]);
+			tmpResult=getResponse(XFFT,F2,tolerance);
+			results[0][1]=tmpResult[0];
+			results[1][1]=tmpResult[1];
+			
+			tmpResult=getResponse(XFFT,Fres,tolerance);
+			results[0][2]=tmpResult[0];
+			results[1][2]=tmpResult[1];
+			
+			f1Data[fIndex]=F2;
 			f1Data[fIndex+1]=Math.round(results[1][0]);
-			f2Data[fIndex]=Math.round(results[0][1]);
+			
+			f2Data[fIndex]=F2;
 			f2Data[fIndex+1]=Math.round(results[1][1]);
-			DPOAEData[fIndex]=Math.round(results[0][2]);
+			
+			DPOAEData[fIndex]=F2;
 			DPOAEData[fIndex+1]=Math.round(results[1][2]);
-			noiseFloor[fIndex]=Math.round(results[0][2]-10);
+			
+			noiseFloor[fIndex]=F2;
 			noiseFloor[fIndex+1]=Math.round(results[1][2]-10);
 		}
 		
 		String outFileName2=data_dir+"dpaudiogram.png";		 		
-		System.out.println("f1=" + f1Data[0]+"f2=" + f2Data[0]+"res=" + DPOAEData[0]);
 		System.out.println("f1=" + f1Data[1]+"f2=" + f2Data[1]+"res=" + DPOAEData[1]);
-		System.out.println("f1=" + f1Data[2]+"f2=" + f2Data[2]+"res=" + DPOAEData[2]);
+		System.out.println("f1=" + f1Data[3]+"f2=" + f2Data[3]+"res=" + DPOAEData[3]);
+		System.out.println("f1=" + f1Data[5]+"f2=" + f2Data[5]+"res=" + DPOAEData[5]);
 		PlotAudiogram audiogram=new PlotAudiogram("DPGram",DPOAEData,noiseFloor,f1Data,f2Data,outFileName2);
 		System.out.println("Analysis complete! ");
 		System.exit(0);
