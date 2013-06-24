1edc8c211ece3c0e5f638ecdd6f4f2aef5d5e97b
ikarosilva00@gmail.com <ikarosilva00@gmail.com@8a878f76-cdcb-badc-6536-4bb3146487dd>
2013-06-24 04:05:18 +0000

    fixed issues with spectral estimation (now in dB SPL)

---
+++ src/org/audiopulse/analysis/DPOAEExplorer.java
@@ -0,0 +1,42 @@
+package org.audiopulse.analysis;
+
+import java.io.IOException;
+
+import org.audiopulse.io.ShortFile;
+import org.audiopulse.ui.SpectralPlotFrame;
+import org.jfree.ui.RefineryUtilities;
+
+//Toy class to help explore and debug the DPOAE analysis 
+//
+public class DPOAEExplorer {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 */
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		int Fs=16000, M=512;
+		String filename="/home/ikaro/APData/opt/k4.raw";
+		short[] rawData = ShortFile.readFile(filename);	
+		double[] audioData=new double[rawData.length];
+		double f=1000;
+		for(int n=0;n<rawData.length;n++){
+			audioData[n]=(double) rawData[n];
+			//audioData[n]=Math.sin(2*Math.PI*f*n/Fs);
+			//rawData[n]= (short) ( 0.05*Math.sin(2*Math.PI*f*n/Fs)*(Short.MAX_VALUE-1));
+		}
+		
+		PlotEpochsTEOAE mplot2= new PlotEpochsTEOAE("stim"
+				,audioData,null,Fs);
+
+		double[][]Pxx=DPOAEAnalysis.getSpectrum(rawData,Fs,M);
+		double Fres=(int) 2*M/Fs;
+		SpectralPlotFrame demo = new SpectralPlotFrame("spec",Pxx,Fres,"Test");
+		demo.pack();
+		RefineryUtilities.centerFrameOnScreen(demo);
+		demo.setVisible(true);
+		
+	}
+
+}
