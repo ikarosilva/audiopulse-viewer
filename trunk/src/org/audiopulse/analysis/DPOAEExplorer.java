bdc6fcd37996877057d11c76b25f633a87211a80
ikarosilva00@gmail.com <ikarosilva00@gmail.com@8a878f76-cdcb-badc-6536-4bb3146487dd>
2013-07-12 03:59:55 +0000

    selecting optimum parameters for dpoae testing

--- trunk/src/org/audiopulse/analysis/DPOAEExplorer.java
+++ trunk/src/org/audiopulse/analysis/DPOAEExplorer.java
@@ -33,28 +33,25 @@ public class DPOAEExplorer {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		int Fs=16000, M=512;
-		String data_dir="/home/ikaro/APData/sweep10db";
-
+		String data_dir="/home/ikaro/APData/htc/l4k";
+		double f2=4000;
 		File[] audioFiles=finder(data_dir);
 		Arrays.sort(audioFiles);
 		short[] rawData;
 		ArrayList<Double> rms= new ArrayList<Double>();
-		double f2=2000;
 		double f1=f2/1.2;
-		double desF=f2;//2*f1-f2;
-		
+		double desF=2*f1-f2;
+
 		for(int i=0;i<audioFiles.length;i++){              
 
-			if(audioFiles[i].getName().contains("2kHz")){
-				rawData=ShortFile.readFile(audioFiles[i].getAbsolutePath());
-				rawData=Arrays.copyOfRange(rawData,Fs,rawData.length);
-				SpectralPlotFrame demo = new SpectralPlotFrame(audioFiles[i].getName(),rawData,Fs,M);
+			rawData=ShortFile.readFile(audioFiles[i].getAbsolutePath());
+			rawData=Arrays.copyOfRange(rawData,Fs,rawData.length);
+			SpectralPlotFrame demo = new SpectralPlotFrame(audioFiles[i].getName(),rawData,Fs,M);
 
-				System.out.println("desF= " +desF);
-				double[] resp= DPOAEAnalysis.getResponse(demo.getSpectrum(),desF,50);
-				rms.add(resp[1]);
-				System.out.println( audioFiles[i] +" spl= " + resp[1]);
-			}
+			System.out.println("desF= " +desF);
+			double[] resp= DPOAEAnalysis.getResponse(demo.getSpectrum(),desF,50);
+			rms.add(resp[1]);
+			System.out.println( audioFiles[i] +" spl= " + resp[1]);
 		}
 		double[] data=new double[rms.size()];
 		for(int j=0;j<rms.size();j++)
