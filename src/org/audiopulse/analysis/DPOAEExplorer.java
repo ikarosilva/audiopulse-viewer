9174b3392868989e153cca703dca57b05405ae2d
ikarosilva00@gmail.com <ikarosilva00@gmail.com@8a878f76-cdcb-badc-6536-4bb3146487dd>
2013-07-22 02:32:28 +0000

    fixed issue with weighted averaging

--- src/org/audiopulse/analysis/DPOAEExplorer.java
+++ src/org/audiopulse/analysis/DPOAEExplorer.java
@@ -35,14 +35,18 @@ public class DPOAEExplorer {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		int Fs=16000, M=512;
-		int f=4;
-		double[] SPL={30,40,50,60,70};
-		//double[] SPL={50,55,60,65,70};
-		String[] sbj={"s1l","s2l","s3l","s4l","s5l","s6l"
-				      ,"s1r","s2r","s3r","s4r","s5r","s6r","cc"};
+		int f=3;
+		double[] SPL=new double[5];
+		if(f==4){
+			SPL[0]=30;SPL[1]=40;SPL[2]=50;SPL[3]=60;SPL[4]=70;
+		}else{
+			SPL[0]=50;SPL[1]=55;SPL[2]=60;SPL[3]=65;SPL[4]=70;
+		}
+		String[] sbj={"s1l","s2l","s3l","s4l","s5l","s6l","s7l"
+				,"s1r","s2r","s3r","s4r","s5r","s6r","s7r","cc"};
 		double[] mopt=new double[sbj.length];
 		double[] bopt=new double[sbj.length];
-		
+
 		for(int s=0;s<sbj.length;s++){
 			String data_dir="/home/ikaro/APData/csail/" + sbj[s]+ Integer.toString(f)+ "k";
 			double f2=1000.0*f;
@@ -57,12 +61,20 @@ public class DPOAEExplorer {
 				//for(int i=3;i<4;i++){ 
 				rawData=ShortFile.readFile(audioFiles[i].getAbsolutePath());
 				rawData=Arrays.copyOfRange(rawData,Fs,rawData.length);
-				SpectralPlotFrame demo = new SpectralPlotFrame(audioFiles[i].getName(),rawData,Fs,M);
 
-				//System.out.println("desF= " +desF);
+				//used for debuggin
+				/*
+				for(int n=0;n<rawData.length;n++){
+                    //audioData[n]=(double) rawData[n];
+                    rawData[n]= (short) (Math.sin(2*Math.PI*desF*n/Fs)*(Short.MAX_VALUE/10.0));
+                    if(n>=0 && n< 1*M )
+                    	rawData[n]= (short) (rawData[n]*10.0);
+				}
+				 */
+
+				SpectralPlotFrame demo = new SpectralPlotFrame(audioFiles[i].getName(),rawData,Fs,M);
 				double[] resp= DPOAEAnalysis.getResponse(demo.getSpectrum(),desF,50);
 				rms.add(resp[1]);
-				//System.out.println( audioFiles[i] +" spl= " + resp[1]);
 			}
 			double[][] data=new double[rms.size()][2];
 			for(int j=0;j<rms.size();j++){
@@ -77,17 +89,17 @@ public class DPOAEExplorer {
 			/*
 			PlotEpochsTEOAE mplot4= new PlotEpochsTEOAE("stimulus"
 					,data,null,1);
-			*/
+			 */
 			mopt[s]=regression.getIntercept();
 			bopt[s]=regression.getSlope();
-			
-			System.out.println(sbj[s] + "  " + mopt[s] +", " + bopt[s]);
 
+			System.out.println(sbj[s] + "  " + mopt[s] +", " + bopt[s]);
+			//break;
 		}
-		
+
 		//Print descriminationa values values
 		ScatterPlot demo = new ScatterPlot("Stats", mopt, bopt);
-		
+
 	}
 
 
